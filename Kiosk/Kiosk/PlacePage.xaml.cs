37a64ab039ce6c21aaabff56034dfe0018ec2384
happlyin <54892429+happlyin@users.noreply.github.com>
2020-09-25 11:06:11 +0900

    [UPDATE] Place

--- Kiosk/Kiosk/PlacePage.xaml.cs
+++ Kiosk/Kiosk/PlacePage.xaml.cs
@@ -16,11 +16,11 @@ using System.Windows.Shapes;
 namespace Kiosk
 {
     /// <summary>
-    /// PlacrPage.xaml에 대한 상호 작용 논리
+    /// PlacePage.xaml에 대한 상호 작용 논리
     /// </summary>
-    public partial class PlacrPage : Page
+    public partial class PlacePage : Page
     {
-        public PlacrPage()
+        public PlacePage()
         {
             InitializeComponent();
         }
