ae32603a3a7ed1b889c5803ab0d7ce6a41c16ff9
박진 <50200481+flaw5886@users.noreply.github.com>
2020-10-06 12:19:38 +0900

    [ADD] FoodList

--- Kiosk/Kiosk/OrderPage.xaml.cs
+++ Kiosk/Kiosk/OrderPage.xaml.cs
@@ -24,13 +24,26 @@ namespace Kiosk
         public OrderPage()
         {
             InitializeComponent();
+            xCategory.SelectedIndex = 0;
         }
 
-        private List<Food> lstFood = new List<Food>()
+        private List<Food> foodList = new List<Food>()
         {
+             new Food(){ category = Category.BURGER, name = "1955", imagePath = @"res/burger/1955.png" },
+             new Food(){ category = Category.BURGER, name = "치즈", imagePath = @"res/burger/cheese.png" },
 
+             new Food(){ category = Category.CHICKEN, name = "ham", imagePath = @"res/burger/ham.png" },
+             new Food(){ category = Category.CHICKEN, name = "mcChicken", imagePath = @"res/burger/1955.png" },
 
+             new Food(){ category = Category.DRINK, name = "BigMac", imagePath = @"res/burger/BIgMac.png" },
+             new Food(){ category = Category.DRINK, name = "double1955", imagePath = @"res/burger/double1955.png" },
         };
+
+        private void xCategory_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            Category category = (Category)xCategory.SelectedIndex;
+            xMenus.ItemsSource = foodList.Where(x => x.category == category).ToList();
+        }
 
         private void Order_Cancel_Click(object sender, RoutedEventArgs e)
         {
